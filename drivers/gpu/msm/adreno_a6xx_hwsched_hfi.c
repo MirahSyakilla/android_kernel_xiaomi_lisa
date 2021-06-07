60bad18302ed32629b060194fc769f7883585d31
Harshdeep Dhatt <hdhatt@codeaurora.org>
2021-06-07 16:38:33 -0600

    BACKPORT: msm: kgsl: Add memory barriers around hfi queue accesses
    The read/write pointer updates may be re-ordered w.r.t actual
    reads/writes of the hfi queues. Hence, add appropriate barriers
    to avoid cases where GMU sees a stale or incorrect contents of
    these queues.
    
    Change-Id: I20a5ea949dca8d17133ffdd66b77d53c17d21401
    Signed-off-by: Harshdeep Dhatt <hdhatt@codeaurora.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    (cherry picked from commit 4f5755094d208c34ee728cfdfa3a0bdbedbf7146)

--- drivers/gpu/msm/adreno_a6xx_hwsched_hfi.c
+++ drivers/gpu/msm/adreno_a6xx_hwsched_hfi.c
@@ -1244,16 +1244,6 @@ skipib:
 	if (ret)
 		goto free;
 
-	/*
-	 * Memory barrier to make sure packet and write index are written before
-	 * an interrupt is raised
-	 */
-	wmb();
-
-	add_profile_events(adreno_dev, drawobj, &time);
-
-	cmdobj->submit_ticks = time.ticks;
-
 	/* Send interrupt to GMU to receive the message */
 	gmu_core_regwrite(KGSL_DEVICE(adreno_dev), A6XX_GMU_HOST2GMU_INTR_SET,
 		DISPQ_IRQ_BIT(drawobj->context->gmu_dispatch_queue));
