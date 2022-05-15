0103606a6593d07d614ae6676361a71ed9f0df07
Stephan Mueller <smueller@chronox.de>
2022-05-15 16:39:02 +0200

    scheduler - add entropy sampling hook
    The scheduler can be used as a source of entropy. This requires the
    presence of a hook that invokes the entropy source implementation.
    
    When the scheduler-based entropy source is not compiled, the hook is
    folded into a noop which does not affect the scheduler in any way.
    
    Signed-off-by: Stephan Mueller <smueller@chronox.de>
    Change-Id: I3afc1b7103ca8292c6bf6b8d356caa4a23ce362e
    (cherry picked from commit 76d116c19dc1f72675c485d7b661edae0c95492c)
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6,6 +6,11 @@
  *
  *  Copyright (C) 1991-2002  Linus Torvalds
  */
+#include <linux/lrng.h>
+#include <linux/highmem.h>
+#include <linux/hrtimer_api.h>
+#include <linux/ktime_api.h>
+
 #include "sched.h"
 
 #include <linux/nospec.h>
@@ -2382,6 +2387,8 @@ static void
 ttwu_stat(struct task_struct *p, int cpu, int wake_flags)
 {
 	struct rq *rq;
+
+	add_sched_randomness(p, cpu);
 
 	if (!schedstat_enabled())
 		return;
