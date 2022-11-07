a3e95bb00934de12907a56cec44c30e73712782b
Liam Howlett <liam.howlett@oracle.com>
2022-11-07 16:38:35 +0000

    maple_tree: fix depth tracking in maple_state
    It is possible to confuse the depth tracking in the maple state by
    searching the same node for values.  Fix the depth tracking by moving
    where the depth is incremented closer to where the node changes level. 
    Also change the initial depth setting when using the root node.
    
    Link: https://lkml.kernel.org/r/20221107163814.866612-1-Liam.Howlett@oracle.com
    Signed-off-by: Liam R. Howlett <Liam.Howlett@oracle.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- lib/maple_tree.c
+++ lib/maple_tree.c
@@ -1352,6 +1352,7 @@ static inline struct maple_enode *mas_start(struct ma_state *mas)
 		root = mas_root(mas);
 		/* Tree with nodes */
 		if (likely(xa_is_node(root))) {
+			mas->depth = 1;
 			mas->node = mte_safe_root(root);
 			return NULL;
 		}
@@ -3728,7 +3729,6 @@ static bool mas_is_span_wr(struct ma_wr_state *wr_mas)
 
 static inline void mas_wr_walk_descend(struct ma_wr_state *wr_mas)
 {
-	wr_mas->mas->depth++;
 	wr_mas->type = mte_node_type(wr_mas->mas->node);
 	mas_wr_node_walk(wr_mas);
 	wr_mas->slots = ma_slots(wr_mas->node, wr_mas->type);
@@ -3740,6 +3740,7 @@ static inline void mas_wr_walk_traverse(struct ma_wr_state *wr_mas)
 	wr_mas->mas->min = wr_mas->r_min;
 	wr_mas->mas->node = wr_mas->content;
 	wr_mas->mas->offset = 0;
+	wr_mas->mas->depth++;
 }
 /*
  * mas_wr_walk() - Walk the tree for a write.
