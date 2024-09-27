51792f42c13d6f11f5845af376403fa455ff6bc9
Tejun Heo <tj@kernel.org>
2024-09-27 13:46:12 -1000

    sched/core: Add ENQUEUE_RQ_SELECTED to indicate whether ->select_task_rq() was called
    During ttwu, ->select_task_rq() can be skipped if only one CPU is allowed or
    migration is disabled. sched_ext schedulers may perform operations such as
    direct dispatch from ->select_task_rq() path and it is useful for them to
    know whether ->select_task_rq() was skipped in the ->enqueue_task() path.
    
    Currently, sched_ext schedulers are using ENQUEUE_WAKEUP for this purpose
    and end up assuming incorrectly that ->select_task_rq() was called for tasks
    that are bound to a single CPU or migration disabled.
    
    Make select_task_rq() indicate whether ->select_task_rq() was called by
    setting WF_RQ_SELECTED in *wake_flags and make ttwu_do_activate() map that
    to ENQUEUE_RQ_SELECTED for ->enqueue_task().
    
    This will be used by sched_ext to fix ->select_task_rq() skip detection.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Acked-by: David Vernet <void@manifault.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1794,12 +1794,20 @@ static inline int task_on_rq_migrating(struct task_struct *p)
 	return READ_ONCE(p->on_rq) == TASK_ON_RQ_MIGRATING;
 }
 
-/*
- * wake flags
- */
-#define WF_SYNC			0x01		/* Waker goes to sleep after wakeup */
-#define WF_FORK			0x02		/* Child wakeup after fork */
-#define WF_MIGRATED		0x4		/* Internal use, task got migrated */
+/* Wake flags. The first three directly map to some SD flag value */
+#define WF_EXEC     0x02 /* Wakeup after exec; maps to SD_BALANCE_EXEC */
+#define WF_FORK     0x04 /* Wakeup after fork; maps to SD_BALANCE_FORK */
+#define WF_TTWU     0x08 /* Wakeup;            maps to SD_BALANCE_WAKE */
+
+#define WF_SYNC     0x10 /* Waker goes to sleep after wakeup */
+#define WF_MIGRATED 0x20 /* Internal use, task got migrated */
+#define WF_RQ_SELECTED		0x40 /* ->select_task_rq() was called */
+
+#ifdef CONFIG_SMP
+static_assert(WF_EXEC == SD_BALANCE_EXEC);
+static_assert(WF_FORK == SD_BALANCE_FORK);
+static_assert(WF_TTWU == SD_BALANCE_WAKE);
+#endif
 
 /*
  * To aid in avoiding the subversion of "niceness" due to uneven distribution
@@ -1832,6 +1840,7 @@ extern const u32		sched_prio_to_wmult[40];
  * ENQUEUE_HEAD      - place at front of runqueue (tail if not specified)
  * ENQUEUE_REPLENISH - CBS (replenish runtime and postpone deadline)
  * ENQUEUE_MIGRATED  - the task was migrated during wakeup
+ * ENQUEUE_RQ_SELECTED - ->select_task_rq() was called
  *
  */
 
@@ -1852,6 +1861,11 @@ extern const u32		sched_prio_to_wmult[40];
 #else
 #define ENQUEUE_MIGRATED	0x00
 #endif
+
+#define ENQUEUE_WAKEUP_SYNC	0x80
+#define ENQUEUE_INITIAL		0x100
+#define ENQUEUE_DELAYED		0x200
+#define ENQUEUE_RQ_SELECTED	0x400
 
 #define RETRY_TASK		((void *)-1UL)
 
