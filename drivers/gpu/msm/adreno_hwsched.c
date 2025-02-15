5107e9bbf68fdafb3993fe3aa6ccea2f87b5c38d
Pankaj Gupta <quic_gpankaj@quicinc.com>
2025-02-15 13:26:58 +0530

    BACKPORT: kgsl: Restore original task priority after real-time elevation
    In the hwsched_sendcmd() function, the current task's priority is
    temporarily elevated to real-time to avoid context switches while
    holding the device mutex. However, the priority was being reset to
    the default nice value of 0, which might not be the original priority
    of the task.
    This fix ensures that the original nice value of the task is restored
    after releasing the mutex.
    
    Change-Id: Ie63b5c1d332c04def5f8e0358640fdd2cbdf7627
    Signed-off-by: Pankaj Gupta <quic_gpankaj@quicinc.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    (cherry picked from commit 7450d63f3e78f9c5d8b07d4d1a22d020adc11886)

--- drivers/gpu/msm/adreno_hwsched.c
+++ drivers/gpu/msm/adreno_hwsched.c
@@ -315,9 +315,10 @@ static int hwsched_sendcmd(struct adreno_device *adreno_dev,
 	struct kgsl_drawobj_cmd *cmdobj)
 {
 	struct sched_param sched_param = { .sched_priority = MAX_RT_PRIO / 2 };
+	int nice = task_nice(current);
 	struct sched_attr attr = {
 		.sched_policy = SCHED_NORMAL,
-		.sched_nice   = 0,
+		.sched_nice   = nice,
 	};
 	struct kgsl_device *device = KGSL_DEVICE(adreno_dev);
 	struct adreno_hwsched *hwsched = to_hwsched(adreno_dev);
@@ -342,7 +343,6 @@ static int hwsched_sendcmd(struct adreno_device *adreno_dev,
 		ret = -EBUSY;
 		goto done;
 	}
-
 
 	if (kgsl_context_detached(context)) {
 		ret = -ENOENT;
