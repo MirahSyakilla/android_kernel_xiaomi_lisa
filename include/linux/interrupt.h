313de44876687a1019a5da9baacfe29cc3e37b40
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2025-09-24 21:12:21 +0600

    sched: Merge branch `upstream/sched-6.1` into `redwood`
    • android14-6.1-lts upstream sched source: https://github.com/Atom-X-Devs/scarlet_xiaomi_sm7325
    • Branch: upstream/sched-6.1
    
    • Commit log (2 commits):
      [FIXUP] BACKPORT: sched/core: add forced idle accounting for cgroups
      ANDROID: softirq: add tasklet to LONG_SOFTIRQ_MASK
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- include/linux/interrupt.h
+++ include/linux/interrupt.h
@@ -510,11 +510,11 @@ enum
 
 #define SOFTIRQ_STOP_IDLE_MASK (~(1 << RCU_SOFTIRQ))
 /* Softirq's where the handling might be long: */
-#define LONG_SOFTIRQ_MASK ((1 << NET_TX_SOFTIRQ)       | \
-			   (1 << NET_RX_SOFTIRQ)       | \
-			   (1 << BLOCK_SOFTIRQ)        | \
-			   (1 << IRQ_POLL_SOFTIRQ)     | \
-			   (1 << TASKLET_SOFTIRQ))
+#define LONG_SOFTIRQ_MASK (BIT(NET_TX_SOFTIRQ)    | \
+			   BIT(NET_RX_SOFTIRQ)    | \
+			   BIT(BLOCK_SOFTIRQ)     | \
+			   BIT(IRQ_POLL_SOFTIRQ)  | \
+			   BIT(TASKLET_SOFTIRQ))
 
 /* map softirq index to softirq name. update 'softirq_to_name' in
  * kernel/softirq.c when adding a new softirq.
