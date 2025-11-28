51639f0aad16e68ac0fa312fc8ff96f97a471a95
Thomas Gleixner <tglx@linutronix.de>
2025-11-29 01:05:37 +0900

    timers: Provide timer_shutdown[_sync]()
    [ Upstream commit f571faf6e443b6011ccb585d57866177af1f643c ]
    
    Tearing down timers which have circular dependencies to other
    functionality, e.g. workqueues, where the timer can schedule work and work
    can arm timers, is not trivial.
    
    In those cases it is desired to shutdown the timer in a way which prevents
    rearming of the timer. The mechanism to do so is to set timer->function to
    NULL and use this as an indicator for the timer arming functions to ignore
    the (re)arm request.
    
    Expose new interfaces for this: timer_shutdown_sync() and timer_shutdown().
    
    timer_shutdown_sync() has the same functionality as timer_delete_sync()
    plus the NULL-ification of the timer function.
    
    timer_shutdown() has the same functionality as timer_delete() plus the
    NULL-ification of the timer function.
    
    In both cases the rearming of the timer is prevented by silently discarding
    rearm attempts due to timer->function being NULL.
    
    Co-developed-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Tested-by: Guenter Roeck <linux@roeck-us.net>
    Reviewed-by: Jacob Keller <jacob.e.keller@intel.com>
    Reviewed-by: Anna-Maria Behnsen <anna-maria@linutronix.de>
    Link: https://lore.kernel.org/all/20220407161745.7d6754b3@gandalf.local.home
    Link: https://lore.kernel.org/all/20221110064101.429013735@goodmis.org
    Link: https://lore.kernel.org/r/20221123201625.314230270@linutronix.de
    Signed-off-by: Jeongjun Park <aha310510@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 36bdfa51a1ad719e3e5f954295874b1e9d8fc072)

--- include/linux/timer.h
+++ include/linux/timer.h
@@ -186,6 +186,8 @@ extern int timer_reduce(struct timer_list *timer, unsigned long expires);
 extern void add_timer(struct timer_list *timer);
 
 extern int try_to_del_timer_sync(struct timer_list *timer);
+extern int timer_shutdown_sync(struct timer_list *timer);
+extern int timer_shutdown(struct timer_list *timer);
 
 extern int del_timer_sync(struct timer_list *timer);
 
