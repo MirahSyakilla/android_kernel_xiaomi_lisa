8554e65f7a15742f7c2c4a25225bfb5c96819b92
Mirah Syakilla <mirahsyakilla@gmail.com>
2025-12-08 23:03:51 +0000

    lib/maple_tree: Fixed maple tree update.
    This commit fixed issue introduced by
    commit a180209.

--- lib/maple_tree.c
+++ lib/maple_tree.c
@@ -5535,10 +5535,13 @@ static void mt_destroy_walk(struct maple_enode *enode, unsigned char ma_flags,
 	struct maple_enode *start;
 	struct maple_tree mt;
 
+	MA_STATE(mas, &mt, 0, 0);
+
 	if (mte_is_leaf(enode)) {
 		mte_set_node_dead(enode);
 		node->type = mte_node_type(enode);
 		goto free_leaf;
+	}
 
 	mt_init_flags(&mt, ma_flags);
 	mas_lock(&mas);
