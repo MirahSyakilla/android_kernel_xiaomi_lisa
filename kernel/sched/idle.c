8c533fd5f1c5be6f38097cfaae4ec8e261b16699
Vincent Guittot <vincent.guittot@linaro.org>
2026-01-21 17:33:17 +0100

    sched fair fix pelt clock synchronization on idle entry

--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -400,6 +400,12 @@ static void set_next_task_idle(struct rq *rq, struct task_struct *next, bool fir
 	update_idle_core(rq);
 	schedstat_inc(rq->sched_goidle);
 	queue_core_balance(rq);
+
+	/*
+	 * rq is about to be idle, check if we need to update the
+	 * lost_idle_time of clock_pelt
+	 */
+	update_idle_rq_clock_pelt(rq);
 }
 
 #ifdef CONFIG_SMP
