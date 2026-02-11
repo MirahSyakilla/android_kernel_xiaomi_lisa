ab9d3345d2f7759e0af6a7b12e4993943559d3a0
Mirah Syakilla <mirahsyakilla@gmail.com>
2026-02-11 13:20:20 +0800

    SuSFS: Reimplemented patches to latest.

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -3365,10 +3365,7 @@ static inline int vfs_fstatat(int dfd, const char __user *filename,
 	return vfs_statx(dfd, filename, flags | AT_NO_AUTOMOUNT,
 			 stat, STATX_BASIC_STATS);
 }
-static inline int vfs_fstat(int fd, struct kstat *stat)
-{
-	return vfs_statx_fd(fd, stat, STATX_BASIC_STATS, 0);
-}
+extern int vfs_fstat(int fd, struct kstat *stat);
 
 
 extern const char *vfs_get_link(struct dentry *, struct delayed_call *);
--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -31,6 +31,12 @@
 #include "avc_ss.h"
 #include "classmap.h"
 
+#ifdef CONFIG_KSU_SUSFS
+extern u32 susfs_ksu_sid;
+extern u32 susfs_priv_app_sid;
+bool susfs_is_avc_log_spoofing_enabled = false;
+#endif
+
 #define AVC_CACHE_SLOTS			512
 #define AVC_DEF_CACHE_THRESHOLD		512
 #define AVC_CACHE_RECLAIM		16
@@ -719,6 +725,17 @@ static void avc_audit_post_callback(struct audit_buffer *ab, void *a)
 
 	rc = security_sid_to_context(sad->state, sad->tsid, &scontext,
 				     &scontext_len);
+#ifdef CONFIG_KSU_SUSFS
+	if (unlikely(sad->tsid == susfs_ksu_sid && susfs_is_avc_log_spoofing_enabled)) {
+		if (rc) {
+			audit_log_format(ab, " tsid=%d", susfs_priv_app_sid);
+		} else {
+			kfree(scontext);
+			audit_log_format(ab, " tcontext=%s", "u:r:priv_app:s0:c512,c768");
+		}
+		goto bypass_orig_flow;
+	}
+#endif
 	if (rc)
 		audit_log_format(ab, " tsid=%d", sad->tsid);
 	else {
@@ -726,6 +743,9 @@ static void avc_audit_post_callback(struct audit_buffer *ab, void *a)
 		kfree(scontext);
 	}
 
+#ifdef CONFIG_KSU_SUSFS
+bypass_orig_flow:
+#endif
 	audit_log_format(ab, " tclass=%s", secclass_map[sad->tclass-1].name);
 
 	if (sad->denied)
