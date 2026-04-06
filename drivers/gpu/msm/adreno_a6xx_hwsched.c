6c0883861e1daa21a104eb9c07e36035a3dc3737
Mirah Syakilla <mirahsyakilla@gmail.com>
2026-04-07 00:33:59 +0800

    fix topology and kgsl API mismatches for 5.4 base

--- drivers/gpu/msm/adreno_a6xx_hwsched.c
+++ drivers/gpu/msm/adreno_a6xx_hwsched.c
@@ -670,8 +670,7 @@ static void hwsched_idle_check(struct work_struct *work)
 	if (test_bit(GMU_DISABLE_SLUMBER, &device->gmu_core.flags))
 		goto done;
 
-	if (!atomic_read(&device->active_cnt) &&
-		time_is_before_eq_jiffies(device->idle_jiffies)) {
+	if (!atomic_read(&device->active_cnt)) {
 		if (!a6xx_hw_isidle(adreno_dev)) {
 			dev_err(device->dev, "GPU isn't idle before SLUMBER\n");
 			gmu_fault_snapshot(device);
