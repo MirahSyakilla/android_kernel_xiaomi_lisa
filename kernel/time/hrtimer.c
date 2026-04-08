51ad646150200dd9b1d07cef6447ab448dbeb594
Mirah Syakilla <mirahsyakilla@gmail.com>
2026-04-09 02:32:32 +0800

    hrtimers: always update CPU base in retrigger_next_event
    Unconditionally refresh the target CPU hrtimer base so migrated softirq timers are not stranded when high-res mode is inactive.

--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -720,9 +720,6 @@ static void retrigger_next_event(void *arg)
 {
 	struct hrtimer_cpu_base *base = this_cpu_ptr(&hrtimer_bases);
 
-	if (!__hrtimer_hres_active(base))
-		return;
-
 	raw_spin_lock(&base->lock);
 	hrtimer_update_base(base);
 	hrtimer_force_reprogram(base, 0);
